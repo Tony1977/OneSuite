e00a5c3ab2592ae8f8a69d3bad562e085951230a
Tony1977 <tarunbounthiyal@hotmail.com>
2022-07-15 10:59:37 +0530

    UserProject added

---
+++ com.onesuite/API/UserManagement/src/test/java/com/OneSuite/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OneSuite.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
