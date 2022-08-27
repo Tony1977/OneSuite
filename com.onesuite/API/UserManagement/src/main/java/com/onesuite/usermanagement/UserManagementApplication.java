3b51dade7ae874da59722a6276cbf013a937126d
Tony1977 <tarunbounthiyal@hotmail.com>
2022-08-27 11:57:22 +0530

    UI project with  docker composer

--- com.onesuite/API/UserManagement/src/main/java/com/onesuite/usermanagement/UserManagementApplication.java
+++ com.onesuite/API/UserManagement/src/main/java/com/onesuite/usermanagement/UserManagementApplication.java
@@ -2,8 +2,10 @@ package com.onesuite.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserManagementApplication {
 
